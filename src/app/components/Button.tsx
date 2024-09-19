31cb3d939b7bb944eccdbf98f306a1b8a2e0cf33
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2024-09-19 15:31:25 +0200

    improve button styles

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,10 +1,24 @@
 import { styled } from "../../../styled-system/jsx";
+import { SystemStyleObject } from "../../../styled-system/types";
+
+const transition: SystemStyleObject = {
+  transitionDuration: "0.15s",
+  transitionTimingFunction: "ease-out",
+};
 
 export const Button = styled("button", {
   base: {
     backgroundColor: "blue.100",
     padding: "2rem 8rem",
     cursor: "pointer",
+    border: "1px solid token(colors.blue.200)",
+
+    ...transition,
+    transitionProperty: "background-color",
+
+    _hover: {
+      backgroundColor: "blue.200",
+    },
 
     _disabled: {
       backgroundColor: "gray.200",
