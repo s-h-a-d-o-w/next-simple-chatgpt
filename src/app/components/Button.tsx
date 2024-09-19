78a71a83b86fb87589410777d2c23b358ab6ba4e
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2024-09-19 15:11:37 +0200

    tweak button style

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -3,8 +3,7 @@ import { styled } from "../../../styled-system/jsx";
 export const Button = styled("button", {
   base: {
     backgroundColor: "blue.100",
-    borderRadius: "4rem",
-    padding: "2rem 4rem",
+    padding: "2rem 8rem",
     cursor: "pointer",
 
     _disabled: {
