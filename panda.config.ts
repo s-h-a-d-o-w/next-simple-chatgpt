235078ed219c2829b703c930c091eec6de4547d7
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2024-09-19 15:10:56 +0200

    hash css names in production bundle

--- panda.config.ts
+++ panda.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
+  hash: process.env.NODE_ENV === "production",
+
   // Whether to use css reset
   preflight: true,
 
-  // Where to look for your css declarations
   include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
-
-  // Files to exclude
-  exclude: [],
+  // exclude: [],
 
   theme: {
     extend: {
