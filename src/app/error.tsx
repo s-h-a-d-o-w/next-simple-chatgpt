cbf2cca2a203f19eecb327f63bc85425312c75dc
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2024-09-22 22:05:10 +0200

    Log history for convenicence in root error component

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,9 +1,23 @@
 "use client";
 
 import { Centered } from "@/components/Centered";
+import useLocalStorageState from "use-local-storage-state";
+import { type Message as MessageType } from "ai/react";
+import superjson from "superjson";
 
 export default function Error() {
   // In a commercial project, we would obviously send error details to some error tracking service.
+
+  const [conversationHistory] = useLocalStorageState<Array<MessageType[]>>(
+    "history",
+    {
+      defaultValue: [],
+      serializer: superjson,
+    },
+  );
+
+  console.log("Maybe the history got corrupted?");
+  console.log(conversationHistory);
 
   return (
     <Centered>
