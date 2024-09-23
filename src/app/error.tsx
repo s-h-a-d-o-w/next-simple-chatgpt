ce271278514e66d584c207353b1fa87b0f4c2d24
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2024-09-23 12:57:04 +0200

    Don't use local storage hook in `error`

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,27 +1,16 @@
 "use client";
 
 import { Centered } from "@/components/Centered";
-import useLocalStorageState from "use-local-storage-state";
-import { type Message as MessageType } from "ai/react";
-import superjson from "superjson";
 
 export default function Error() {
   // In a commercial project, we would obviously send error details to some error tracking service.
 
-  const [conversationHistory] = useLocalStorageState<Array<MessageType[]>>(
-    "history",
-    {
-      defaultValue: [],
-      serializer: superjson,
-    },
-  );
-
   console.log("Maybe the history got corrupted?");
-  console.log(conversationHistory);
+  console.log(localStorage.getItem("history"));
 
   return (
     <Centered>
-      <div style={{ maxWidth: "200rem", textAlign: "center" }}>
+      <div style={{ width: "75%", maxWidth: "400rem", textAlign: "center" }}>
         If this was a commercial project, I&apos;d say something like: &quot;We
         have encountered an unexpected error. It has been reported automatically
         but if you keep experiencing this please reach out to support.&quot;
