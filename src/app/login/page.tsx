7e126946bf78fcf4a3edbfba37b0f9c1097e1005
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2024-10-22 20:10:00 +0200

    Add login disclaimer

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,8 @@
+import Link from "next/link";
 import { styled } from "../../../styled-system/jsx";
 import { AuthButton } from "./components/AuthButton";
 
-const StyledCenter = styled("main", {
+const StyledMain = styled("main", {
   base: {
     position: "absolute",
     width: "100%",
@@ -13,10 +14,31 @@ const StyledCenter = styled("main", {
   },
 });
 
+const StyledContainer = styled("div", {
+  base: {
+    maxWidth: "400rem",
+    display: "flex",
+    flexDirection: "column",
+    gap: "24rem",
+  },
+});
+
 export default function Login() {
   return (
-    <StyledCenter>
-      <AuthButton />
-    </StyledCenter>
+    <StyledMain>
+      <StyledContainer>
+        <h1>This is a private app.</h1>
+        <p>
+          If you aren&apos;t on the whitelist, you will not be able to sign in!
+          At the moment, I recommend everybody run their own instance of this -
+          see{" "}
+          <Link href="https://github.com/s-h-a-d-o-w/next-simple-chatgpt">
+            README
+          </Link>
+          .
+        </p>
+        <AuthButton />
+      </StyledContainer>
+    </StyledMain>
   );
 }
