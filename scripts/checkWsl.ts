e9eedbc3348825642d4dcecb906c3670919d9a67
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2024-11-04 23:19:38 +0100

    Make WSL message stand out more

--- scripts/checkWsl.ts
+++ scripts/checkWsl.ts
@@ -4,6 +4,8 @@ if (
   Boolean(process.env.WSL_DISTRO_NAME) ||
   os.release().toLowerCase().includes("microsoft")
 ) {
+  console.log("===============================");
   console.log("WSL is not supported.");
+  console.log("===============================");
   process.exit(1);
 }
