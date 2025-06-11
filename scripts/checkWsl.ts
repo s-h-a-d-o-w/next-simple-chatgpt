bc2806af0b2bff88e3da10ee2968e21cf8d9b708
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2025-06-11 14:53:22 +0200

    Simplify typescript configs

--- scripts/checkWsl.ts
+++ scripts/checkWsl.ts
@@ -1,7 +1,7 @@
 import os from "os";
 
 if (
-  Boolean(process.env.WSL_DISTRO_NAME) ||
+  Boolean(process.env["WSL_DISTRO_NAME"]) ||
   os.release().toLowerCase().includes("microsoft")
 ) {
   console.log("===============================");
