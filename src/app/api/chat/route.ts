cbff86d56719197ffe93f808eb17355807997714
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2025-07-18 15:53:44 +0200

    Add caching for anthropic

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -14,6 +14,40 @@ export const maxDuration = 60;
 const whitelist = process.env["WHITELIST"]?.split(",");
 const isTest = Boolean(process.env["CI"]);
 
+function convertMessagesAnthropic(messages: Message[]) {
+  const coreMessages = convertToCoreMessages(messages);
+
+  const relevantIndices = coreMessages
+    .map((msg, index) =>
+      msg.role === "user" || msg.role === "tool" ? index : -1,
+    )
+    .filter((index) => index !== -1)
+    // take last 2 user/tool messages
+    .slice(-2);
+  // definitely always include the last user message
+  const lastUserMsgIndex = coreMessages.findLastIndex(
+    (msg) => msg.role === "user",
+  );
+  // it's fine if it's -1 or duplicate, since we will simply do an inclusion check
+  relevantIndices.push(lastUserMsgIndex);
+
+  const messagesWithCacheMarkers = coreMessages.map((message, index) => {
+    if (relevantIndices.includes(index)) {
+      return {
+        ...message,
+        providerOptions: {
+          anthropic: {
+            cacheControl: { type: "ephemeral" },
+          },
+        },
+      };
+    }
+    return message;
+  });
+
+  return messagesWithCacheMarkers;
+}
+
 export const POST = auth(async (req) => {
   const isUserWhitelisted = whitelist?.includes(String(req.auth?.user?.email));
   if (!isTest && !isUserWhitelisted) {
@@ -24,7 +58,10 @@ export const POST = auth(async (req) => {
   const modelConfig = models[model];
   const result = streamText({
     model: modelConfig.provider === "openai" ? openai(model) : anthropic(model),
-    messages: convertToCoreMessages(messages),
+    messages:
+      modelConfig.provider === "openai"
+        ? convertToCoreMessages(messages)
+        : convertMessagesAnthropic(messages),
     providerOptions: ["o3-mini", "o4-mini"].includes(model)
       ? {
           openai: {
