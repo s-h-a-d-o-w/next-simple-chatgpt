f55a93cc186d6211a97a07826268198faa7745d5
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2025-11-16 11:56:41 +0100

    Upgrade next.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,18 +3,16 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 import { includeIgnoreFile } from "@eslint/compat";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
-
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...nextVitals,
+  ...nextTs,
   includeIgnoreFile(gitignorePath),
   {
     ignores: ["public/**"],
