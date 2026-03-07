ca8f717c943c58f86c44d0f2639c7e2fedebaf03
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2026-03-07 18:30:52 +0100

    Refactor AuthButtonClient

--- src/app/login/components/AuthButton/AuthButtonClient.tsx
+++ src/app/login/components/AuthButton/AuthButtonClient.tsx
@@ -1,33 +1,30 @@
 "use client";
-// Runs on the client so that we can display a spinner while login/logout is being processed
 
-import { IconButton } from "@/components/IconButton";
-import { type ReactNode, useActionState } from "react";
-import Spinner from "@/components/Spinner";
+import { useFormStatus } from "react-dom";
 import { signIn, signOut } from "./authActions";
 
-function PendingServerAction({ children }: { children: ReactNode }) {
-  const [, , pending] = useActionState(signIn, undefined);
+type ButtonProps = {
+  isSignedIn?: boolean;
+};
 
-  return pending ? <Spinner /> : children;
+function SubmitButton({ isSignedIn }: Required<ButtonProps>) {
+  const { pending } = useFormStatus();
+  console.log(new Date().toISOString(), "pending", pending);
+  return (
+    <button
+      type="submit"
+      disabled={pending}
+      className={`bg-${isSignedIn ? "red" : "blue"}-500 text-white px-4 py-2 rounded-md`}
+    >
+      {pending ? "Loading..." : isSignedIn ? "Sign out" : "Sign in"}
+    </button>
+  );
 }
 
-export function AuthButtonClient({
-  isSignedIn = false,
-}: {
-  isSignedIn?: boolean;
-}) {
-  const [, action] = useActionState(isSignedIn ? signOut : signIn, undefined);
-
+export function AuthButtonClient({ isSignedIn = false }: ButtonProps) {
   return (
-    <form action={action}>
-      <PendingServerAction>
-        {isSignedIn ? (
-          <IconButton name="logout" iconSize="md" label="Sign out" />
-        ) : (
-          <IconButton name="github" iconSize="md" label="Sign in with GitHub" />
-        )}
-      </PendingServerAction>
+    <form action={isSignedIn ? signOut : signIn}>
+      <SubmitButton isSignedIn={isSignedIn} />
     </form>
   );
 }
