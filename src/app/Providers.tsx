3a820a9029805da8f1ef53a68bdca4650c72a03d
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2026-03-15 19:56:29 +0100

    Try to fix session refresh

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { authClient } from "@/lib/utils/authClient";
 import type { ReactNode } from "react";
 import { useState } from "react";
 
@@ -9,6 +10,9 @@ type Props = {
 };
 
 export function Providers({ children }: Props) {
+  // Possibly refresh session on the client. (Usage in RSC and API can't do that.)
+  authClient.useSession();
+
   const [queryClient] = useState(
     () =>
       new QueryClient({
