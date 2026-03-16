b390c8d82ac76a7d4d5e6be44f025e74b3b0e2a0
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2026-03-16 12:37:24 +0100

    Use dummy auth values during e2e tests

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,11 @@
 import { betterAuth, type BetterAuthOptions } from "better-auth";
+import { isTest } from "@/lib/utils/consts";
 
 const authConfig: BetterAuthOptions = {
-  baseURL: process.env["AUTH_URL"]!,
-  secret: process.env["AUTH_SECRET"]!,
+  baseURL: isTest ? "http://localhost:3000" : process.env["AUTH_URL"]!,
+  secret: isTest
+    ? "something long so that better-auth doesn't complain."
+    : process.env["AUTH_SECRET"]!,
   socialProviders: {
     github: {
       clientId: process.env["AUTH_GITHUB_ID"]!,
