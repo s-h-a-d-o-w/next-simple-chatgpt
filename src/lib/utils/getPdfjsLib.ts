1210acfb51e4c3b865cf678f217792b2ed31e0c2
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2026-03-16 18:06:39 +0100

    Remove pdfjs-dist worker from repo

--- src/lib/utils/getPdfjsLib.ts
+++ src/lib/utils/getPdfjsLib.ts
@@ -1,8 +1,10 @@
 let pdfjsLib: typeof import("pdfjs-dist") | undefined;
 
+// Load pdfjs-dist dynamically so that we don't get "Warning: Please use the `legacy` build in Node.js environments." on the server.
 export async function getPdfjsLib() {
   if (!pdfjsLib) {
     pdfjsLib = await import("pdfjs-dist");
+    // Needs to be reachable for the client, don't know how to bundle this.
     pdfjsLib.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.mjs";
   }
   return pdfjsLib;
